92897b48eb4c06baea3ecad97c14ad303b7f9ef8
enrichosamb <187468259+enrichosamb@users.noreply.github.com>
2025-03-25 22:56:02 +0700

    feat: add changes sign in

--- app/(root)/(tabs)/_layout.tsx
+++ app/(root)/(tabs)/_layout.tsx
@@ -1,29 +1,53 @@
 import { Tabs } from "expo-router";
 import React from "react";
-import { Platform } from "react-native";
+import { Image, Platform, Text, View } from "react-native";
 
 import { HapticTab } from "@/components/HapticTab";
 import { IconSymbol } from "@/components/ui/IconSymbol";
 import TabBarBackground from "@/components/ui/TabBarBackground";
 import { Colors } from "@/constants/Colors";
 import { useColorScheme } from "@/hooks/useColorScheme";
+import icons from "@/constants/icons";
 
 export default function TabLayout() {
     const colorScheme = useColorScheme();
 
+    const TabIcon = ({ focused, icon, title }: { focused: boolean; icon: any; title: string; }) => {
+        return(
+            <View className="flex-1 mt-3 flex flex-col items-center">
+                <Image
+                    source={icon}
+                    tintColor={focused ? "#0061FF" : "#666876"}
+                    resizeMode="contain"
+                    className="size-6"
+                />
+                <Text className={`${focused ? "text-primary-300 font-rubik-medium" : "text-black-200 font-rubik"} text-xs w-full text-center mt-1`}>
+                    {title}
+                </Text>
+            </View>
+        )
+    }
+
     return (
         <Tabs
             screenOptions={{
-                tabBarActiveTintColor: Colors[colorScheme ?? "light"].tint,
-                headerShown: false,
-                tabBarButton: HapticTab,
-                tabBarBackground: TabBarBackground,
+                // tabBarButton: HapticTab,
+                // tabBarBackground: TabBarBackground,
+                // tabBarActiveTintColor: Colors[colorScheme ?? "light"].tint,
+                // headerShown: false,
+                tabBarShowLabel: false,
                 tabBarStyle: Platform.select({
                     ios: {
                         // Use a transparent background on iOS to show the blur effect
                         position: "absolute",
                     },
-                    default: {},
+                    default: {
+                        backgroundColor: "white",
+                        position: "absolute",
+                        borderTopColor: "#0061FF1A",
+                        borderTopWidth: 1,
+                        minHeight: 70,
+                    },
                 }),
             }}
         >
@@ -31,8 +55,9 @@ export default function TabLayout() {
                 name="index"
                 options={{
                     title: "Home",
+                    headerShown: false,
                     tabBarIcon: ({ color }) => (
-                        <IconSymbol size={28} name="house.fill" color={color} />
+                        <TabIcon icon={icons.home} focused={false} title="Home" />
                     ),
                 }}
             />
@@ -40,12 +65,19 @@ export default function TabLayout() {
                 name="explore"
                 options={{
                     title: "Explore",
+                    headerShown: false,
                     tabBarIcon: ({ color }) => (
-                        <IconSymbol
-                            size={28}
-                            name="paperplane.fill"
-                            color={color}
-                        />
+                        <TabIcon icon={icons.search} focused={false} title="Explore" />
+                    ),
+                }}
+            />
+            <Tabs.Screen
+                name="profile"
+                options={{
+                    title: "Profile",
+                    headerShown: false,
+                    tabBarIcon: ({ color }) => (
+                        <TabIcon icon={icons.person} focused={false} title="Profile" />
                     ),
                 }}
             />
--- app/(root)/(tabs)/index.tsx
+++ app/(root)/(tabs)/index.tsx
@@ -1,20 +1,29 @@
-import { Text, View } from 'react-native';
+import { Image, Text, TouchableOpacity, View } from 'react-native';
 import { Link } from 'expo-router';
+import { SafeAreaView } from 'react-native-safe-area-context';
+import images from '@/constants/images';
+import icons from '@/constants/icons';
 
 export default function HomeScreen() {
     return (
-        <View
-            style={{
-                flex: 1,
-                justifyContent: "center",
-                alignItems: "center"
-            }}
-        >
-            <Text className='text-3xl my-10'>Welcome to ReState</Text>
-            <Link href="/sign-in">Sign In</Link>
-            <Link href="/explore">Explore</Link>
-            <Link href="/profile">Profile</Link>
-            <Link href="/properties/1">Properties - 1</Link>
-        </View>
+        <SafeAreaView className='bg-white h-full'>
+            <View className='px-5'>
+                <View className='flex flex-row items-center justify-between mt-5'>
+                    <View className='flex flex-row items-center gap-2'>
+                        <TouchableOpacity>
+                            <Image source={images.avatar} className='size-12 rounded-full' />
+                        </TouchableOpacity>
+                        <View className='flex flex-col items-start ml-2 justify-center'>
+                            <Text className='text-xs font-rubik text-black-100'>Good Moring</Text>
+                            <Text className='text-base font-rubik-medium text-black-300'>Enricho Alkalas</Text>
+                        </View>
+                    </View>
+                    <TouchableOpacity>
+                        <Image source={icons.bell} className='size-6 mr-2' />
+                    </TouchableOpacity>
+                </View>
+                {/* <Text className='text-3xl my-10'>Welcome to Re-Estate</Text> */}
+            </View>
+        </SafeAreaView>
     );
 }
